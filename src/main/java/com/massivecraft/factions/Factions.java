0a4bbc17f5f6fd4bf7187ef7a5a3905f29aaf978
Mark Hughes <m@rkhugh.es>
2017-02-14 18:21:21 +1100

    Add get and getAll to Factions class, minor refractor

--- src/main/java/com/massivecraft/factions/Factions.java
+++ src/main/java/com/massivecraft/factions/Factions.java
@@ -3,11 +3,75 @@ package com.massivecraft.factions;
 import com.massivecraft.factions.zcore.persist.json.JSONFactions;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
-public abstract class Factions {
-    protected static Factions instance = getFactionsImpl();
+import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 
+public abstract class Factions {
+    private static Factions instance = getImpl();
+    
+    private static Factions getImpl() {
+        switch (Conf.backEnd) {
+            case JSON:
+                return new JSONFactions();
+        }
+        return null;
+    }
+    
+    public static Factions getInstance() { return instance; }
+    
+    
+    public static Faction get(Object o) {
+    	Faction faction = null;
+    	
+    	// Quick conversions
+    	if (o instanceof Player) {
+    		o = FPlayers.getInstance().getByPlayer((Player) o);
+    	}
+    	
+    	if (o instanceof OfflinePlayer) {
+    		o = FPlayers.getInstance().getByOfflinePlayer((OfflinePlayer) o);
+    	}
+    	
+    	// String can be either id or tag
+    	if (o instanceof String) {
+    		// search by id first
+    		faction = instance.getFactionById((String) o);
+    		
+    		if (faction != null) {
+    			return faction;
+    		}
+    		
+    		// now try its tag
+    		return instance.getByTag((String) o);
+    	}
+    	
+    	if (o instanceof FPlayer) {
+    		FPlayer fplayer = (FPlayer) o;
+    		return fplayer.getFaction();
+    	}
+    	
+    	return null;
+    }
+    
+    public List<Faction> getAll(World world) {
+    	List<Faction> all = new ArrayList<Faction>();
+    	
+    	for (Faction faction : instance.getAllFactions()) {
+    		for(FLocation loc : faction.getAllClaims()) {
+    			if (loc.getWorld() != world) continue;
+    			
+    			all.add(faction);
+    			break;
+    		}
+    	}
+    	
+    	return all;
+    }
+    
     public abstract Faction getFactionById(String id);
 
     public abstract Faction getByTag(String str);
@@ -39,17 +103,6 @@ public abstract class Factions {
 
     public abstract void forceSave(boolean sync);
 
-    public static Factions getInstance() {
-        return instance;
-    }
-
-    private static Factions getFactionsImpl() {
-        switch (Conf.backEnd) {
-            case JSON:
-                return new JSONFactions();
-        }
-        return null;
-    }
 
     public abstract void load();
 }
