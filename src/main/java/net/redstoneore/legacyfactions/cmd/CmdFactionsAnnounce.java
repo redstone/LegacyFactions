7d550d5dbb740223073853f04902181ad65981cc
Mark Hughes <m@rkhugh.es>
2017-04-28 13:56:06 +1000

    Remove library dependency by using StringJoiner, use ‘this’ where possible

--- src/main/java/net/redstoneore/legacyfactions/cmd/CmdFactionsAnnounce.java
+++ src/main/java/net/redstoneore/legacyfactions/cmd/CmdFactionsAnnounce.java
@@ -1,6 +1,6 @@
 package net.redstoneore.legacyfactions.cmd;
 
-import org.apache.commons.lang.StringUtils;
+import java.util.StringJoiner;
 
 import net.redstoneore.legacyfactions.Permission;
 import net.redstoneore.legacyfactions.Lang;
@@ -33,7 +33,10 @@ public class CmdFactionsAnnounce extends FCommand {
     @Override
     public void perform() {
     	// Compile announcement
-        String message = StringUtils.join(args, " ");
+        StringJoiner messageJoiner = new StringJoiner(" ", "", "");
+        this.args.forEach(messageJoiner::add);
+
+        String message = messageJoiner.toString();
         
         String announcement = Lang.COMMAND_ANNOUNCE_TEMPALTE.toString()
         		.replaceAll("<tag>", this.myFaction.getTag())
@@ -41,12 +44,12 @@ public class CmdFactionsAnnounce extends FCommand {
         		.replaceAll("<message>", message);
         
         // Notify online players
-        myFaction.getFPlayersWhereOnline(true).forEach(
+        this.myFaction.getFPlayersWhereOnline(true).forEach(
         	fplayer -> fplayer.msg(announcement)
         );
         
         // Store announcement for offline players
-        myFaction.getFPlayersWhereOnline(false).forEach(
+        this.myFaction.getFPlayersWhereOnline(false).forEach(
         	fplayer -> this.myFaction.addAnnouncement(fplayer, announcement)
         );
     }
