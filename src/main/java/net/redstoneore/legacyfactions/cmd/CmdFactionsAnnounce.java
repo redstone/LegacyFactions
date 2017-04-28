976effcc9a2e8ce9cf3dea6135774ba1a47d2db1
Mark Hughes <m@rkhugh.es>
2017-04-28 13:58:32 +1000

    Reformat

--- src/main/java/net/redstoneore/legacyfactions/cmd/CmdFactionsAnnounce.java
+++ src/main/java/net/redstoneore/legacyfactions/cmd/CmdFactionsAnnounce.java
@@ -11,52 +11,52 @@ public class CmdFactionsAnnounce extends FCommand {
 	// CONSTRUCT
 	// -------------------------------------------------- //
 	
-    public CmdFactionsAnnounce() {
-        this.aliases.add("announce");
-        this.aliases.add("ann");
-        
-        this.requiredArgs.add("message");
-        this.errorOnToManyArgs = false;
-        
-        this.permission = Permission.ANNOUNCE.node;
-        this.disableOnLock = false;
-        
-        this.senderMustBePlayer = true;
-        this.senderMustBeMember = true;
-        this.senderMustBeModerator = true;
-    }
-    
+	public CmdFactionsAnnounce() {
+		this.aliases.add("announce");
+		this.aliases.add("ann");
+		
+		this.requiredArgs.add("message");
+		this.errorOnToManyArgs = false;
+		
+		this.permission = Permission.ANNOUNCE.node;
+		this.disableOnLock = false;
+		
+		this.senderMustBePlayer = true;
+		this.senderMustBeMember = true;
+		this.senderMustBeModerator = true;
+	}
+	
 	// -------------------------------------------------- //
 	// METHODS
 	// -------------------------------------------------- //
 
-    @Override
-    public void perform() {
-    	// Compile announcement
-        StringJoiner messageJoiner = new StringJoiner(" ", "", "");
-        this.args.forEach(messageJoiner::add);
+	@Override
+	public void perform() {
+		// Compile announcement
+		StringJoiner messageJoiner = new StringJoiner(" ", "", "");
+		this.args.forEach(messageJoiner::add);
 
-        String message = messageJoiner.toString();
-        
-        String announcement = Lang.COMMAND_ANNOUNCE_TEMPALTE.toString()
-        		.replaceAll("<tag>", this.myFaction.getTag())
-        		.replaceAll("<player>", this.me.getName())
-        		.replaceAll("<message>", message);
-        
-        // Notify online players
-        this.myFaction.getFPlayersWhereOnline(true).forEach(
-        	fplayer -> fplayer.msg(announcement)
-        );
-        
-        // Store announcement for offline players
-        this.myFaction.getFPlayersWhereOnline(false).forEach(
-        	fplayer -> this.myFaction.addAnnouncement(fplayer, announcement)
-        );
-    }
+		String message = messageJoiner.toString();
+		
+		String announcement = Lang.COMMAND_ANNOUNCE_TEMPLATE.toString()
+				.replaceAll("<tag>", this.myFaction.getTag())
+				.replaceAll("<player>", this.me.getName())
+				.replaceAll("<message>", message);
+		
+		// Notify online players
+		this.myFaction.getFPlayersWhereOnline(true).forEach(
+			fplayer -> fplayer.msg(announcement)
+		);
+		
+		// Store announcement for offline players
+		this.myFaction.getFPlayersWhereOnline(false).forEach(
+			fplayer -> this.myFaction.addAnnouncement(fplayer, announcement)
+		);
+	}
 
-    @Override
-    public String getUsageTranslation() {
-        return Lang.COMMAND_ANNOUNCE_DESCRIPTION.toString();
-    }
+	@Override
+	public String getUsageTranslation() {
+		return Lang.COMMAND_ANNOUNCE_DESCRIPTION.toString();
+	}
 
 }
