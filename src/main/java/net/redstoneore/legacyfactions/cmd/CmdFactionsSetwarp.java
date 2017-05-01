47f41dc8262cca20a8d1d66c721ccdf39d4c3787
Mark Hughes <m@rkhugh.es>
2017-05-01 10:31:16 +1000

    New event: EventFactionsWarpCreate

--- src/main/java/net/redstoneore/legacyfactions/cmd/CmdFactionsSetwarp.java
+++ src/main/java/net/redstoneore/legacyfactions/cmd/CmdFactionsSetwarp.java
@@ -4,7 +4,7 @@ import net.redstoneore.legacyfactions.Permission;
 import net.redstoneore.legacyfactions.Relation;
 import net.redstoneore.legacyfactions.Lang;
 import net.redstoneore.legacyfactions.entity.Conf;
-import net.redstoneore.legacyfactions.entity.FPlayer;
+import net.redstoneore.legacyfactions.event.EventFactionsWarpCreate;
 import net.redstoneore.legacyfactions.util.LazyLocation;
 
 public class CmdFactionsSetwarp extends FCommand {
@@ -25,16 +25,24 @@ public class CmdFactionsSetwarp extends FCommand {
 
     @Override
     public void perform() {
+    	// Make sure they can set here
         if (!(fme.getRelationToLocation() == Relation.MEMBER)) {
             fme.msg(Lang.COMMAND_SETFWARP_NOTCLAIMED);
             return;
         }
-
+        
+        // Check the limit 
         if (Conf.warpsMax <= myFaction.warps().size()) {
             fme.msg(Lang.COMMAND_SETFWARP_LIMIT, Conf.warpsMax);
             return;
         }
 
+        // Determine other information for warp 
+        String warpName = argAsString(0);
+        LazyLocation warpLocation = new LazyLocation(fme.getPlayer().getLocation());
+        Double warpCost = Conf.warpCost.get("set");
+        
+        // Check for password
         String warpPassword = argAsString(1);
         if (warpPassword != null && warpPassword.trim() != "") {
         	
@@ -43,19 +51,29 @@ public class CmdFactionsSetwarp extends FCommand {
         		return;
         	}
         	
+        	// Passwords are not case sensitive
         	warpPassword = warpPassword.toLowerCase().trim();
         }
         
-        if (!transact(fme)) return;
-
-        String warpName = argAsString(0);
-        LazyLocation location = new LazyLocation(fme.getPlayer().getLocation());
-        myFaction.warps().setWarp(warpName, location, warpPassword);
+        // If in admin bypass, charge nothing
+        if (this.fme.isAdminBypassing()) warpCost = 0.0;
+        
+        // Call our event
+        EventFactionsWarpCreate event = new EventFactionsWarpCreate(this.myFaction, this.fme, warpName, warpPassword, warpLocation, warpCost);
+        event.call();
+        if (event.isCancelled()) return;
+        
+        // Check for new cost, and pay for it if required
+        if (warpCost > 0 && !this.fme.isAdminBypassing() && !payForCommand(Conf.warpCost.get("set"), Lang.COMMAND_SETFWARP_TOSET.toString(), Lang.COMMAND_SETFWARP_FORSET.toString())) return;
+        
+        // Get new values from event
+        warpName = event.getName();
+        warpPassword = event.getPassword();
+        warpLocation = event.getLocation();
+        
+        // Set the warp
+        myFaction.warps().setWarp(warpName, warpLocation, warpPassword);
         fme.msg(Lang.COMMAND_SETFWARP_SET, warpName);
-    }
-
-    private boolean transact(FPlayer player) {
-        return Conf.warpCost.get("set") == 0 || player.isAdminBypassing() || payForCommand(Conf.warpCost.get("set"), Lang.COMMAND_SETFWARP_TOSET.toString(), Lang.COMMAND_SETFWARP_FORSET.toString());
     }
 
     @Override
---
+++ src/main/java/net/redstoneore/legacyfactions/event/EventFactionsWarpCreate.java
@@ -0,0 +1,92 @@
+package net.redstoneore.legacyfactions.event;
+
+import org.bukkit.Location;
+import org.bukkit.event.Cancellable;
+
+import net.redstoneore.legacyfactions.entity.FPlayer;
+import net.redstoneore.legacyfactions.entity.Faction;
+import net.redstoneore.legacyfactions.util.LazyLocation;
+
+/**
+ * This event is called when a player creates a warp.
+ */
+public class EventFactionsWarpCreate extends AbstractFactionsPlayerEvent implements Cancellable {
+	
+	// -------------------------------------------------- //
+	// CONSTRUCT
+	// -------------------------------------------------- //
+	
+	public EventFactionsWarpCreate(Faction faction, FPlayer fplayer, String warpName, String warpPassword, LazyLocation warpLocation, Double warpCost) {
+		super(faction, fplayer);
+		
+		this.warpName = warpName;
+		this.warpPassword = warpPassword;
+		this.warpLocation = warpLocation;
+		this.warpCost = warpCost;
+	}
+	
+	// -------------------------------------------------- //
+	// FIELDS
+	// -------------------------------------------------- //
+	
+	private Boolean cancelled = false;
+	private String warpName = null;
+	private String warpPassword = null;
+	private LazyLocation warpLocation = null;
+	private Double warpCost;
+	
+	// -------------------------------------------------- //
+	// METHODS
+	// -------------------------------------------------- //
+	
+	@Override
+	public boolean isCancelled() {
+		return this.cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancel) {
+		this.cancelled = cancel;
+	}
+	
+	public void setName(String name) {
+		this.warpName = name;
+	}
+	
+	public String getName() {
+		return this.warpName;
+	}
+	
+	public void setPassword(String password) {
+		this.warpPassword = password;
+	}
+
+	/**
+	 * Returns the password of the warp, can be null if none set
+	 * @return the password of the warp, can be null if none set
+	 */
+	public String getPassword() {
+		return this.warpPassword;
+	}
+	
+	public void setLocation(Location location) {
+		this.warpLocation = new LazyLocation(location);
+	}
+	
+	public void setLocation(LazyLocation location) {
+		this.warpLocation = location;
+	}
+	
+	public LazyLocation getLocation() {
+		return this.warpLocation;
+	}
+	
+	public void setCost(Double cost) {
+		this.warpCost = cost;
+	}
+	
+	public Double getCost() {
+		return this.warpCost;
+	}
+	
+}
