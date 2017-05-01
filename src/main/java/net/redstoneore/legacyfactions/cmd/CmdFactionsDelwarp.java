91ee86b51ce0b7765ef1f2580808719cef8958ec
Mark Hughes <m@rkhugh.es>
2017-05-01 10:55:53 +1000

    New event: EventFactionsWarpDelete

--- src/main/java/net/redstoneore/legacyfactions/cmd/CmdFactionsDelwarp.java
+++ src/main/java/net/redstoneore/legacyfactions/cmd/CmdFactionsDelwarp.java
@@ -5,17 +5,18 @@ import java.util.Optional;
 import net.redstoneore.legacyfactions.Permission;
 import net.redstoneore.legacyfactions.Lang;
 import net.redstoneore.legacyfactions.entity.Conf;
-import net.redstoneore.legacyfactions.entity.FPlayer;
+import net.redstoneore.legacyfactions.event.EventFactionsWarpDelete;
 import net.redstoneore.legacyfactions.warp.FactionWarp;
 
 public class CmdFactionsDelwarp extends FCommand {
 
     public CmdFactionsDelwarp() {
-        super();
         this.aliases.add("delwarp");
         this.aliases.add("dw");
         this.aliases.add("deletewarp");
+        
         this.requiredArgs.add("warp name");
+
         this.senderMustBeMember = true;
         this.senderMustBeModerator = true;
         this.senderMustBePlayer = true;
@@ -27,18 +28,22 @@ public class CmdFactionsDelwarp extends FCommand {
         String name = argAsString(0);
         Optional<FactionWarp> owarp = myFaction.warps().get(name);
         if (owarp.isPresent()) {
-            if (!transact(fme)) {
-                return;
-            }
-            owarp.get().delete();
+            FactionWarp warp = owarp.get();
+            Double cost = Conf.warpCost.get("delete");
+            
+            if (fme.isAdminBypassing()) cost = 0.0;
+            
+            EventFactionsWarpDelete event = new EventFactionsWarpDelete(myFaction, fme, warp, cost);
+            event.call();
+            if (event.isCancelled()) return;
+            
+            if (cost > 0 && !fme.isAdminBypassing() && !this.payForCommand(Conf.warpCost.get("delete"), Lang.COMMAND_DELFWARP_TODELETE.toString(), Lang.COMMAND_DELFWARP_FORDELETE.toString())) return;
+            
+            warp.delete();
             fme.msg(Lang.COMMAND_DELFWARP_DELETED, name);
         } else {
             fme.msg(Lang.COMMAND_DELFWARP_INVALID, name);
         }
-    }
-
-    private boolean transact(FPlayer player) {
-        return Conf.warpCost.get("delete") == 0 || player.isAdminBypassing() || payForCommand(Conf.warpCost.get("delete"), Lang.COMMAND_DELFWARP_TODELETE.toString(), Lang.COMMAND_DELFWARP_FORDELETE.toString());
     }
 
     @Override
---
+++ src/main/java/net/redstoneore/legacyfactions/event/EventFactionsWarpDelete.java
@@ -0,0 +1,56 @@
+package net.redstoneore.legacyfactions.event;
+
+import org.bukkit.event.Cancellable;
+
+import net.redstoneore.legacyfactions.entity.FPlayer;
+import net.redstoneore.legacyfactions.entity.Faction;
+import net.redstoneore.legacyfactions.warp.FactionWarp;
+
+public class EventFactionsWarpDelete extends AbstractFactionsPlayerEvent implements Cancellable {
+	
+	// -------------------------------------------------- //
+	// CONSTRUCT
+	// -------------------------------------------------- //
+	
+	public EventFactionsWarpDelete(Faction faction, FPlayer fplayer, FactionWarp warp, Double cost) {
+		super(faction, fplayer);
+		
+		this.warp = warp;
+		this.cost = cost;
+	}
+	
+	// -------------------------------------------------- //
+	// FIELDS
+	// -------------------------------------------------- //
+	
+	private Boolean cancelled = false;
+	private FactionWarp warp = null;
+	private Double cost = 0.0;
+	
+	// -------------------------------------------------- //
+	// METHODS
+	// -------------------------------------------------- //
+	
+	@Override
+	public boolean isCancelled() {
+		return this.cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancel) {
+		this.cancelled = cancel;
+	}
+	
+	public FactionWarp getWarp() {
+		return this.warp;
+	}
+	
+	public void setCost(Double cost) {
+		this.cost = cost;
+	}
+	
+	public Double getCost() {
+		return this.cost;
+	}
+	
+}
