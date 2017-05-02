e4898a88f9dac433d500eeabdf180cd8777c5a5f
Mark Hughes <m@rkhugh.es>
2017-05-02 23:09:06 +1000

    Formatting

--- src/main/java/net/redstoneore/legacyfactions/util/DiscUtil.java
+++ src/main/java/net/redstoneore/legacyfactions/util/DiscUtil.java
@@ -4,7 +4,11 @@ import org.bukkit.Bukkit;
 
 import net.redstoneore.legacyfactions.Factions;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
@@ -12,122 +16,124 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class DiscUtil {
 
-    // -------------------------------------------- //
-    // CONSTANTS
-    // -------------------------------------------- //
+	// -------------------------------------------------- //
+	// CONSTANTS
+	// -------------------------------------------------- //
 
-    private final static String UTF8 = "UTF-8";
+	private final static String UTF8 = "UTF-8";
 
-    // -------------------------------------------- //
-    // BYTE
-    // -------------------------------------------- //
+	// -------------------------------------------------- //
+	// BYTE
+	// -------------------------------------------------- //
+	
+	public static byte[] readBytes(File file) throws IOException {
+		Integer length = (int) file.length();
+		byte[] output = new byte[length];
+		Integer offset = 0;
+		
+		InputStream in = new FileInputStream(file);
+		
+		while (offset < length) {
+			offset += in.read(output, offset, (length - offset));
+		}
+		
+		in.close();
+		
+		return output;
+	}
 
+	public static void writeBytes(File file, byte[] bytes) throws IOException {
+		File parent = file.getParentFile();
+		if (parent != null && !parent.exists()) parent.mkdirs();
+		FileOutputStream out = new FileOutputStream(file);
+		out.write(bytes);
+		out.close();
+	}
 
-    public static byte[] readBytes(File file) throws IOException {
-        int length = (int) file.length();
-        byte[] output = new byte[length];
-        InputStream in = new FileInputStream(file);
-        int offset = 0;
-        while (offset < length) {
-            offset += in.read(output, offset, (length - offset));
-        }
-        in.close();
-        return output;
-    }
+	// -------------------------------------------------- //
+	// STRING
+	// -------------------------------------------------- //
 
-    public static void writeBytes(File file, byte[] bytes) throws IOException {
-        FileOutputStream out = new FileOutputStream(file);
-        out.write(bytes);
-        out.close();
-    }
+	public static void write(File file, String content) throws IOException {
+		writeBytes(file, utf8(content));
+	}
 
-    // -------------------------------------------- //
-    // STRING
-    // -------------------------------------------- //
+	public static String read(File file) throws IOException {
+		return utf8(readBytes(file));
+	}
 
-    public static void write(File file, String content) throws IOException {
-        writeBytes(file, utf8(content));
-    }
+	// -------------------------------------------------- //
+	// CATCH
+	// -------------------------------------------------- //
 
-    public static String read(File file) throws IOException {
-        return utf8(readBytes(file));
-    }
+	private static HashMap<String, Lock> locks = new HashMap<>();
 
-    // -------------------------------------------- //
-    // CATCH
-    // -------------------------------------------- //
+	public static Boolean writeCatch(final File file, final String content, boolean sync) {
+		String name = file.getName();
+		final Lock lock;
 
-    private static HashMap<String, Lock> locks = new HashMap<String, Lock>();
+		// Create lock for each file if there isn't already one.
+		if (locks.containsKey(name)) {
+			lock = locks.get(name);
+		} else {
+			ReadWriteLock rwl = new ReentrantReadWriteLock();
+			lock = rwl.writeLock();
+			locks.put(name, lock);
+		}
 
-    public static boolean writeCatch(final File file, final String content, boolean sync) {
-        String name = file.getName();
-        final Lock lock;
+		if (sync) {
+			lock.lock();
+			try {
+				write(file, content);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				lock.unlock();
+			}
+		} else {
+			Bukkit.getScheduler().runTaskAsynchronously(Factions.get(), () -> {
+				lock.lock();
+				try {
+					write(file, content);
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					lock.unlock();
+				}
+			});
+		}
 
-        // Create lock for each file if there isn't already one.
-        if (locks.containsKey(name)) {
-            lock = locks.get(name);
-        } else {
-            ReadWriteLock rwl = new ReentrantReadWriteLock();
-            lock = rwl.writeLock();
-            locks.put(name, lock);
-        }
+		return true;
+	}
 
-        if (sync) {
-            lock.lock();
-            try {
-            	write(file, content);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                lock.unlock();
-            }
-        } else {
-            Bukkit.getScheduler().runTaskAsynchronously(Factions.get(), new Runnable() {
-                @Override
-                public void run() {
-                    lock.lock();
-                    try {
-                    	write(file, content);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        lock.unlock();
-                    }
-                }
-            });
-        }
+	public static String readCatch(File file) {
+		try {
+			return read(file);
+		} catch (Exception e) {
+			return null;
+		}
+	}
 
-        return true; // don't really care but for some reason this is a boolean.
-    }
+	// -------------------------------------------------- //
+	// UTF8 UTIL
+	// -------------------------------------------------- //
 
-    public static String readCatch(File file) {
-        try {
-            return read(file);
-        } catch (IOException e) {
-            return null;
-        }
-    }
+	public static byte[] utf8(String string) {
+		try {
+			return string.getBytes(UTF8);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    // -------------------------------------------- //
-    // UTF8 ENCODE AND DECODE
-    // -------------------------------------------- //
-
-    public static byte[] utf8(String string) {
-        try {
-            return string.getBytes(UTF8);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public static String utf8(byte[] bytes) {
-        try {
-            return new String(bytes, UTF8);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+	public static String utf8(byte[] bytes) {
+		try {
+			return new String(bytes, UTF8);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
