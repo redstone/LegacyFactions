07f3ab8922245cb7f9fc381517d10db0cfa2e390
Mark Hughes <m@rkhugh.es>
2017-05-03 13:01:17 +1000

    Formatting

--- src/main/java/net/redstoneore/legacyfactions/cmd/CmdFactionsShow.java
+++ src/main/java/net/redstoneore/legacyfactions/cmd/CmdFactionsShow.java
@@ -9,107 +9,87 @@ import net.redstoneore.legacyfactions.entity.Faction;
 import net.redstoneore.legacyfactions.util.TagReplacerUtil;
 import net.redstoneore.legacyfactions.util.TagUtil;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CmdFactionsShow extends FCommand {
+	
+	public CmdFactionsShow() {
+		this.aliases.add("show");
+		this.aliases.add("who");
+		
+		// this.requiredArgs.add("");
+		this.optionalArgs.put("faction tag", "yours");
 
-    List<String> defaults = new ArrayList<String>();
+		this.permission = Permission.SHOW.node;
+		this.disableOnLock = false;
 
-    public CmdFactionsShow() {
-        this.aliases.add("show");
-        this.aliases.add("who");
+		this.senderMustBeMember = false;
+		this.senderMustBeModerator = false;
+		this.senderMustBeAdmin = false;
+	}
 
-        // add defaults to /f show in case config doesnt have it
-        defaults.add("{header}");
-        defaults.add("<a>Description: <i>{description}");
-        defaults.add("<a>Joining: <i>{joining}    {peaceful}");
-        defaults.add("<a>Land / Power / Maxpower: <i> {chunks} / {power} / {maxPower}");
-        defaults.add("<a>Founded: <i>{create-date}");
-        defaults.add("<a>This faction is permanent, remaining even with no members.");
-        defaults.add("<a>Land value: <i>{land-value} {land-refund}");
-        defaults.add("<a>Balance: <i>{balance}");
-        defaults.add("<a>Allies(<i>{allies}<a>/<i>{max-allies}<a>): {allies-list}");
-        defaults.add("<a>Online: (<i>{online}<a>/<i>{members}<a>): {online-list}");
-        defaults.add("<a>Offline: (<i>{offline}<a>/<i>{members}<a>): {offline-list}");
+	@Override
+	public void perform() {
+		Faction faction = myFaction;
+		if (this.argIsSet(0)) {
+			faction = this.argAsFaction(0, null);
+		}
+		
+		if (faction == null) {
+			return;
+		}
 
-        // this.requiredArgs.add("");
-        this.optionalArgs.put("faction tag", "yours");
+		if (!fme.getPlayer().hasPermission("factions.show.bypassexempt")
+				&& Conf.showExempt.contains(faction.getTag())) {
+			msg(Lang.COMMAND_SHOW_EXEMPT);
+			return;
+		}
 
-        this.permission = Permission.SHOW.node;
-        this.disableOnLock = false;
+		// if economy is enabled, they're not on the bypass list, and this command has a cost set, make 'em pay
+		if (!payForCommand(Conf.econCostShow, Lang.COMMAND_SHOW_TOSHOW, Lang.COMMAND_SHOW_FORSHOW)) {
+			return;
+		}
+		
+		if (!faction.isNormal()) {
+			String tag = faction.getTag(fme);
+			// send header and that's all
+			String header = Conf.showLines.get(0);
+			if (TagReplacerUtil.HEADER.contains(header)) {
+				msg(Factions.get().getTextUtil().titleize(tag));
+			} else {
+				msg(Factions.get().getTextUtil().parse(TagReplacerUtil.FACTION.replace(header, tag)));
+			}
+			return; // we only show header for non-normal factions
+		}
 
-        senderMustBeMember = false;
-        senderMustBeModerator = false;
-        senderMustBeAdmin = false;
-    }
+		for (String line : Conf.showLines) {
+			String parsed = TagUtil.parsePlain(faction, fme, line); // use relations
+			
+			if (parsed == null) continue; // Due to minimal f show.
+			
+			if (TagUtil.hasFancy(parsed)) {
+				List<FancyMessage> fancy = TagUtil.parseFancy(faction, fme, parsed);
+				if (fancy == null) continue;
+				sendFancyMessage(fancy);
+				continue;
+			}
+			
+			if (!parsed.contains("{notFrozen}") && !parsed.contains("{notPermanent}")) {
+				if (parsed.contains("{ig}")) {
+					// replaces all variables with no home TL
+					parsed = parsed.substring(0, parsed.indexOf("{ig}")) + Lang.COMMAND_SHOW_NOHOME.toString();
+				}
+				if (parsed.contains("%")) {
+					parsed = parsed.replaceAll("%", ""); // Just in case it got in there before we disallowed it.
+				}
+				msg(Factions.get().getTextUtil().parse(parsed));
+			}
+		}
+	}
 
-    @Override
-    public void perform() {
-        Faction faction = myFaction;
-        if (this.argIsSet(0)) {
-            faction = this.argAsFaction(0);
-        }
-        if (faction == null) {
-            return;
-        }
-
-        if (!fme.getPlayer().hasPermission("factions.show.bypassexempt")
-                && Conf.showExempt.contains(faction.getTag())) {
-            msg(Lang.COMMAND_SHOW_EXEMPT);
-            return;
-        }
-
-        // if economy is enabled, they're not on the bypass list, and this command has a cost set, make 'em pay
-        if (!payForCommand(Conf.econCostShow, Lang.COMMAND_SHOW_TOSHOW, Lang.COMMAND_SHOW_FORSHOW)) {
-            return;
-        }
-
-        List<String> show = Conf.showLines;
-        if (show == null || show.isEmpty()) {
-            show = defaults;
-        }
-
-        if (!faction.isNormal()) {
-            String tag = faction.getTag(fme);
-            // send header and that's all
-            String header = show.get(0);
-            if (TagReplacerUtil.HEADER.contains(header)) {
-                msg(Factions.get().getTextUtil().titleize(tag));
-            } else {
-                msg(Factions.get().getTextUtil().parse(TagReplacerUtil.FACTION.replace(header, tag)));
-            }
-            return; // we only show header for non-normal factions
-        }
-
-        for (String raw : show) {
-            String parsed = TagUtil.parsePlain(faction, fme, raw); // use relations
-            if (parsed == null) {
-                continue; // Due to minimal f show.
-            }
-            if (TagUtil.hasFancy(parsed)) {
-                List<FancyMessage> fancy = TagUtil.parseFancy(faction, fme, parsed);
-                if (fancy != null) {
-                    sendFancyMessage(fancy);
-                }
-                continue;
-            }
-            if (!parsed.contains("{notFrozen}") && !parsed.contains("{notPermanent}")) {
-                if (parsed.contains("{ig}")) {
-                    // replaces all variables with no home TL
-                    parsed = parsed.substring(0, parsed.indexOf("{ig}")) + Lang.COMMAND_SHOW_NOHOME.toString();
-                }
-                if (parsed.contains("%")) {
-                    parsed = parsed.replaceAll("%", ""); // Just in case it got in there before we disallowed it.
-                }
-                msg(Factions.get().getTextUtil().parse(parsed));
-            }
-        }
-    }
-
-    @Override
-    public String getUsageTranslation() {
-        return Lang.COMMAND_SHOW_COMMANDDESCRIPTION.toString();
-    }
+	@Override
+	public String getUsageTranslation() {
+		return Lang.COMMAND_SHOW_COMMANDDESCRIPTION.toString();
+	}
 
 }
