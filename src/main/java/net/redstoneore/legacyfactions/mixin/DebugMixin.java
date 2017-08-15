4ef62e344542f58853200dbde8b3fd1f54f36ae7
Mark Hughes <m@rkhugh.es>
2017-08-15 16:47:07 +1000

    Move debug to mixin

---
+++ src/main/java/net/redstoneore/legacyfactions/mixin/DebugMixin.java
@@ -0,0 +1,58 @@
+package net.redstoneore.legacyfactions.mixin;
+
+import java.util.stream.Collectors;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.Plugin;
+
+import net.redstoneore.legacyfactions.Factions;
+import net.redstoneore.legacyfactions.integration.Integrations;
+
+public class DebugMixin {
+
+	public static void sendToConsole() {
+		ConsoleCommandSender sender = Bukkit.getConsoleSender();
+		
+		sender.sendMessage(ChatColor.WHITE + "=== START DEBUG ===");
+		sender.sendMessage(" ");
+		sender.sendMessage("----- Server Information -----");
+
+		// Show server version.
+		sender.sendMessage("Server Version: " + Bukkit.getServer().getVersion());
+
+		// Return the name and version.
+		sender.sendMessage(Factions.get().getDescription().getName() + " v" + Factions.get().getDescription().getVersion());
+		
+		// Package name.
+		sender.sendMessage("Package: " + DebugMixin.class.getClass().getPackage().toString());
+		
+		// Java Version.
+		sender.sendMessage("Java Version: " + System.getProperty("java.version"));
+		
+		// Show all plugins with their versions and authors.
+		sender.sendMessage("");
+		sender.sendMessage("");
+		sender.sendMessage("----- Plugins -----");
+
+		for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
+			String name = plugin.getDescription().getName();
+			String version = plugin.getDescription().getVersion();
+			String authors = plugin.getDescription().getAuthors().stream().collect(Collectors.joining(", "));
+			sender.sendMessage(name + " v" + version + " by " + authors);
+		}
+		
+		// Show all integrations.
+		sender.sendMessage(" ");
+		sender.sendMessage("----- Integrations -----");
+		
+		Integrations.getAll().forEach(integration -> {
+			sender.sendMessage(integration.getName() + " = " + integration.isEnabled());
+		});
+		
+		sender.sendMessage(" ");
+		sender.sendMessage(ChatColor.WHITE + "=== END DEBUG ===");
+	}
+	
+}
