e22f03e6ef4663af7ec26510045e2cac71f1caec
Mark Hughes <m@rkhugh.es>
2017-08-25 22:39:59 +1000

    Fixes placeholder bug

--- src/main/java/net/redstoneore/legacyfactions/placeholder/FactionsPlaceholders.java
+++ src/main/java/net/redstoneore/legacyfactions/placeholder/FactionsPlaceholders.java
@@ -457,7 +457,9 @@ public class FactionsPlaceholders {
 	 */
 	public String parse(Player player, String string) {
 		for (FactionsPlaceholder placeholder : this.getPlaceholders()) {
-			string = string.replace("{factions_" + placeholder.placeholder() + "}", placeholder.get(player)+"");
+			if (string.contains("{factions_" + placeholder.placeholder() + "}")) {
+				string = string.replace("{factions_" + placeholder.placeholder() + "}", placeholder.get(player)+"");
+			}
 		}
 		return string;
 	}
@@ -473,10 +475,14 @@ public class FactionsPlaceholders {
 			if (placeholder instanceof FactionsPlaceholderRelation) {
 				FactionsPlaceholderRelation placeholderRel = (FactionsPlaceholderRelation) placeholder;
 				
-				string = string.replace("{rel_factions_" + placeholderRel.placeholder() + "}", placeholderRel.get(player1, player2)+"");
+				if (string.contains("{rel_factions_" + placeholderRel.placeholder() + "}")) {
+					string = string.replace("{rel_factions_" + placeholderRel.placeholder() + "}", placeholderRel.get(player1, player2)+"");
+				}
 			} 
 			
-			string = string.replace("{factions_" + placeholder.placeholder() + "}", placeholder.get(player1)+"");
+			if (string.contains("{factions_" + placeholder.placeholder() + "}")) {
+				string = string.replace("{factions_" + placeholder.placeholder() + "}", placeholder.get(player1)+"");
+			}
 		}
 		return string;
 	}
