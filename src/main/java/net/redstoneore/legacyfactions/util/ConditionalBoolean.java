8aa982d0f63640108b425db382e8b00f4d5ac604
Mark Hughes <m@rkhugh.es>
2017-08-31 16:16:54 +1000

    New callback sync

--- src/main/java/net/redstoneore/legacyfactions/util/ConditionalBoolean.java
+++ src/main/java/net/redstoneore/legacyfactions/util/ConditionalBoolean.java
@@ -3,6 +3,10 @@ package net.redstoneore.legacyfactions.util;
 @FunctionalInterface
 public interface ConditionalBoolean {
 	
+	public static ConditionalBoolean of(ConditionalBoolean of) {
+		return of;
+	}
+	
 	boolean get();
 
 }
