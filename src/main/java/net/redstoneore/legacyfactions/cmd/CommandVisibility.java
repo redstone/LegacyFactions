324518a4d0297f8423c95f72ac2496dd403f760c
Mark Hughes <m@rkhugh.es>
2017-09-13 16:19:53 +1000

    Document this class properly

--- src/main/java/net/redstoneore/legacyfactions/cmd/CommandVisibility.java
+++ src/main/java/net/redstoneore/legacyfactions/cmd/CommandVisibility.java
@@ -1,14 +1,29 @@
 package net.redstoneore.legacyfactions.cmd;
 
+/**
+ * Commands can be specified with visibility options.
+ */
 public enum CommandVisibility {
 	
 	// -------------------------------------------------- //
 	// ENUM
 	// -------------------------------------------------- //
 	
-	VISIBLE, // Visible commands are visible to anyone. Even those who don't have permission to use it or is of invalid sender type.
-	SECRET, // Secret commands are visible only to those who can use the command. These commands are usually some kind of admin commands.
-	INVISIBLE, // Invisible commands are invisible to everyone, even those who can use the command.
+	/**
+	 * Visible commands are visible to anyone. Even those who don't have permission to use it or is of invalid sender type.
+	 */
+	VISIBLE,
+	
+	/**
+	 * Secret commands are visible only to those who can use the command. These commands are usually some kind of admin commands.
+	 */
+	SECRET,
+	
+	/**
+	 * Invisible commands are invisible to everyone, even those who can use the command.
+	 */
+	INVISIBLE,
+	
 	;
 	
 }
