29b16a99ca9e98bce02a4ab09df3d89baa7930e0
Mark Hughes <m@rkhugh.es>
2017-10-10 15:31:19 +1100

    Documentation

--- src/main/java/net/redstoneore/legacyfactions/landmanager/LandManager.java
+++ src/main/java/net/redstoneore/legacyfactions/landmanager/LandManager.java
@@ -24,6 +24,10 @@ public interface LandManager {
 	// METHODS
 	// -------------------------------------------------- //
 	
+	/**
+	 * Rebuild a chunk
+	 * @param chunk The chunk to rebuild.
+	 */
 	void rebuild(Chunk chunk);
 	
 }
